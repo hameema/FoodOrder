d58c0c7b445a3320250690c5dae0a90c64129a3f
hamim <hamimahmed35@gmail.com>
2019-06-06 14:42:10 +0200

    lkjh

--- pizza/order/models.py
+++ pizza/order/models.py
@@ -6,10 +6,24 @@ class student (models.Model):
     email = models.CharField(max_length=128)
     password = models.CharField(max_length=225)
 
+    def __str__(self):
+        return f"{self.id} - {self.email} to {self.username}"
+
+
+
+
 class menu (models.Model):
-    dish = models.CharField(max_length=225)
+    desh = models.ManyToManyField(order, blank=True, related_name="passengers")
     prise = models.IntegerField()
 
+    def __str__(self):
+        return f"  {self.desh} to {self.prise}"
+
+
 class order (models.Model):
-    dish = models.CharField(max_length=225)
+    dish = models.ForeignKey(menu, on_delete=models.CASCADE, related_name="order")
+    totle = models.IntegerField()
     note = models.TextField()
+
+    def __str__(self):
+        return f"{self.dish} - {self.totle} to {self.note}"
